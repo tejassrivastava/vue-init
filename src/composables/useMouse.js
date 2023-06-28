69eb8482e8c07a1fb3dc8403bc70b56b271709fb
tejassrivastava <tejas.srivsatava@hotmail.com>
2023-06-28 16:28:53 +0530

    Mixins & Composables 2

--- src/composables/useMouse.js
+++ src/composables/useMouse.js
@@ -7,7 +7,7 @@ export function useMouse() {
   // state encapsulated and managed by the composable
   const x = ref(0)
   const y = ref(0)
-
+  //this
   // a composable can update its managed state over time.
   function update(event) {
     x.value = event.pageX
